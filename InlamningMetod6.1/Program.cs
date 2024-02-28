88c69a3f7fc990e23f5748431f185008eeab42e4
Airblow <143172177+Airblow@users.noreply.github.com>
2024-02-28 09:54:45 +0100

    Add project files.

---
+++ InlamningMetod6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InlamningMetod6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = Addera(3, 5, 8);
+            Console.WriteLine(summa);
+
+        }
+
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            return summa;
+
+        }
+
+    }
+
+}
